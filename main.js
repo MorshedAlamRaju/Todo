e26e7df6adfd6b4e997424edf61af1827838d433
Morshed_Alam_Raju <rajuseu5@gmail.com>
2018-11-27 21:08:44 +0600

    add delete button and done button

--- main.js
+++ main.js
@@ -16,20 +16,36 @@ function attachItems(items) {
 }
 
 function makeListItem(text) {
+
+		var div = document.createElement("div");
+		var div1 = document.createElement("div");
+		var div2 = document.createElement("div");
+    var div3 = document.createElement("div");
+    div.classList.add("row");
+    div1.classList.add("col");
+    div2.classList.add("col"); 
+    div3.classList.add("col");
+    div1.classList.add("s10");
+    div2.classList.add("s1");
+    div3.classList.add("s1");
     var li = document.createElement("li");
     li.classList.add("collection-item");
-    li.classList.add("active");
+    var i = document.createElement("i");
+    i.classList.add("material-icons");
+    i.innerHTML = "check_circle";
+    i.onclick = function(){checkActivity(this)};
+    div2.appendChild(i);
     var a = document.createElement("i");
     a.classList.add("material-icons");
     a.innerHTML = "delete";
-    a.onclick = function() {
-        deleteItem(this);
-    };
-
+    a.onclick = function() {deleteItem(this);};
     var textNode = document.createTextNode(text);
-
-    li.appendChild(textNode);  
-    li.appendChild(a);
+    div1.appendChild(textNode);
+    div3.appendChild(a);
+    div.appendChild(div1);
+    div.appendChild(div2);
+    div.appendChild(div3);
+    li.appendChild(div);
     return li;
 }
 
@@ -58,14 +74,18 @@ function handleClickEvent() {
 }
 
 function deleteItem(anchor) {
-    var item = anchor.parentNode.firstChild.textContent;
+    var item = anchor.parentNode.parentNode.firstChild.textContent;
     var index = items.indexOf(item);
     if(index !== -1) {
         items.splice(index, 1);
         clearUL();
         attachItems(items);
     }
-    console.log(index);
+}
+
+function checkActivity(i){
+	let done = i.parentNode.parentNode.parentNode;
+	done.classList.add("active");
 }
 
 document.querySelector("#add-item").addEventListener("click", handleClickEvent);
@@ -78,4 +98,3 @@ document.addEventListener('keypress', (event) => {
     if(keyName === "Enter")
         handleClickEvent();
   });
-  
